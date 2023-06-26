72680a52a7b7126b25f76b1c1039eca844e98d80
shawn9977 <shawn.zhao@intel.com>
2023-06-26 10:33:37 +0800

    second commit

--- helloIntelDevCloud.dockerfile
+++ helloIntelDevCloud.dockerfile
@@ -4,6 +4,15 @@ FROM python:3.9-alpine
 # Set the working directory
 WORKDIR /app
 
+
+
+ARG RUN_ON_PREM="/mount_folder"
+ENV RUN_ON_PREM=$RUN_ON_PREM
+
+
+
+
+
 # Copy the application code to the container
 COPY helloIntelDevCloud.py .
 
--- helloIntelDevCloud.py
+++ helloIntelDevCloud.py
@@ -1,2 +1,23 @@
+import os
+
 print("Hello, Intel DevCloud World!")
 
+def write_lines_to_file(file_path, line1, line2):
+    with open(file_path, 'w') as file:
+        file.write(line1 + '\n')
+        file.write(line2 + '\n')
+
+# Example usage
+
+
+file_path = os.environ.get('RUN_ON_PREM') + '/performance.txt'
+
+#file_path = 'performance.txt'
+line1 = 'Hello, Intel DevCloud!'
+line2 = 'Welcome to use Intel DevCloud, Throughtput and Latency is .....'
+
+write_lines_to_file(file_path, line1, line2)
+
+with open(file_path, 'r') as file:
+    contents = file.read()
+    print(contents)
